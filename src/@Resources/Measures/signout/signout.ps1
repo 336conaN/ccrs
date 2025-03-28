f7a3dbc3bc85da98ff20d7d8f01d4fbf50adce2d
conaN <dev@conan.moe>
2025-03-28 21:22:09 +0000

    move shutdown, restart, signout and lock to menu

--- src/@Resources/Measures/signout/signout.ps1
+++
@@ -1,8 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-
-$result = [System.Windows.Forms.MessageBox]::Show("Do you want to sign out?", "SignOut Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
-
-if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
-    shutdown /l
-} else {
-}
